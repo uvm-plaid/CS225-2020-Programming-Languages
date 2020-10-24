cdda714e134fbee0aefae33d02447ad0c455d0ee
wintersmind <wintersmind@gmail.com>
2020-10-24 08:33:18 -0400

    Update examples-10-29-20.ml

--- Examples/examples-10-29-20.ml
+++ Examples/examples-10-29-20.ml
@@ -1,7 +1,7 @@
 let rec for_to_do j k f = 
-  if j > k then () else (f j; for_to_do (j+1) k f)
+  	if j > k then () else (f j; for_to_do (j+1) k f)
 
 let fact n =
 	let soln = ref 1 in 
-  (for_to_do 1 n (fun i -> soln := (!soln * i)); 
-   !soln)
+  	(for_to_do 1 n (fun i -> soln := (!soln * i)); 
+   	 !soln)
